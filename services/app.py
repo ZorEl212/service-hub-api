9e5c4cb3939aeab5b4ca019b5af67022ca613cb1
ZorEl212 <yabsirad212@gmail.com>
2025-04-19 14:28:49 -0400

    services: add DBSessionContext and AppCRUD classes for database session management
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

---
+++ services/app.py
@@ -0,0 +1,13 @@
+from cgi import FieldStorage
+
+import models
+from models.engine.file_storage import FileStorage
+
+
+class DBSessionContext(object):
+    def __init__(self):
+        self.db = models.storage
+        print(f"Storage instance: {type(self.db)}")
+
+class AppCRUD(DBSessionContext):
+    pass
