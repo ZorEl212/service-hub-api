1b4625ac59bd2ca0f222404a6de5b028c847b213
ZorEl212 <yabsirad212@gmail.com>
2025-05-09 14:30:17 -0400

    Refactor user schemas to extend FastAPI Users base classes  Add UserUpdate model as well.
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- schemas/user.py
+++ schemas/user.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+from pydantic import EmailStr, Field
+from fastapi_users import schemas
 
 from schemas.base_model import BaseAPIModel
 
 
-class UserCreate(BaseModel):
+class UserCreate(schemas.BaseUserCreate):
     email: EmailStr
     password: str = Field(min_length=6)
     first_name: str = Field(default="")
@@ -13,7 +16,13 @@ class UserCreate(BaseModel):
 
 class User(BaseAPIModel):
     email: EmailStr
-    password: str = Field(min_length=6)
     first_name: str = Field(default="")
     last_name: str = Field(default="")
     username: str = Field(default="")
+
+class UserUpdate(schemas.BaseUserUpdate):
+    email: Optional[EmailStr] = None
+    password: Optional[str] = Field(default=None, min_length=6)
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    username: Optional[str] = None
