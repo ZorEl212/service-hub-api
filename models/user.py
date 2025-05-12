0b9163a209d0843787c9050aab50334e09134e51
ZorEl212 <yabsirad212@gmail.com>
2025-05-12 05:11:34 -0400

    api/models: drop user type specific attributes from `User` collections
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/user.py
+++ models/user.py
@@ -1,28 +1,17 @@
+from typing import Literal
+
 from pydantic import Field
 from fastapi_users_db_beanie import BaseOAuthAccount
 
 from models.base_model import BaseModel
-from schemas.user import UserCreate
 
-from fastapi_users.db import BeanieBaseUser, BeanieUserDatabase
+from fastapi_users.db import BeanieBaseUser
 
 class OAuthAccount(BaseOAuthAccount):
     pass
 
 class User(BeanieBaseUser, BaseModel):
     """User model that defines attributes and methods for user instances"""
+    role: Literal["customer", "provider"]
 
-    first_name: str = ""
-    last_name: str = ""
-    username: str = ""
     oauth_accounts: list[OAuthAccount] = Field(default_factory=list)
-
-    @classmethod
-    def from_create(cls, user: UserCreate) -> "User":
-        """Create a User instance from a UserCreate schema"""
-        return cls(
-            first_name=user.first_name,
-            last_name=user.last_name,
-            username=user.username,
-        )
-
