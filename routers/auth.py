f2112a8d0d9eb36add7d81fb3f809c1955179f1a
ZorEl212 <yabsirad212@gmail.com>
2025-05-16 06:17:29 -0400

    api/routers: Scaffold login route
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,12 @@
+import json
+
 from fastapi import APIRouter, Depends, HTTPException, status, Request
+from fastapi.security import OAuth2PasswordRequestForm
 
 from fastapi_users import exceptions, models, schemas
+from fastapi_users.authentication import AuthenticationBackend, Authenticator, Strategy
 from fastapi_users.manager import BaseUserManager, UserManagerDependency
+from fastapi_users.openapi import OpenAPIResponseType
 from fastapi_users.router.common import ErrorCode, ErrorModel
 
 from services.user import UserCRUD
@@ -81,3 +86,96 @@ class AuthRoutes:
             return schemas.model_validate(user_schema, created_user)
 
         return router
+
+    @classmethod
+    def get_auth_router(cls,
+            backend: AuthenticationBackend[models.UP, models.ID],
+            get_user_manager: UserManagerDependency[models.UP, models.ID],
+            authenticator: Authenticator[models.UP, models.ID],
+            requires_verification: bool = False,
+    ) -> APIRouter:
+        """Generate a router with login/logout routes for an authentication backend."""
+        router = APIRouter()
+        get_current_user_token = authenticator.current_user_token(
+            active=True, verified=requires_verification
+        )
+
+        login_responses: OpenAPIResponseType = {
+            status.HTTP_400_BAD_REQUEST: {
+                "model": ErrorModel,
+                "content": {
+                    "application/json": {
+                        "examples": {
+                            ErrorCode.LOGIN_BAD_CREDENTIALS: {
+                                "summary": "Bad credentials or the user is inactive.",
+                                "value": {"detail": ErrorCode.LOGIN_BAD_CREDENTIALS},
+                            },
+                            ErrorCode.LOGIN_USER_NOT_VERIFIED: {
+                                "summary": "The user is not verified.",
+                                "value": {"detail": ErrorCode.LOGIN_USER_NOT_VERIFIED},
+                            },
+                        }
+                    }
+                },
+            },
+            **backend.transport.get_openapi_login_responses_success(),
+        }
+
+        @router.post(
+            "/login",
+            name=f"auth:{backend.name}.login",
+            responses=login_responses,
+        )
+        async def login(
+                request: Request,
+                credentials: OAuth2PasswordRequestForm = Depends(),
+                user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+                strategy: Strategy[models.UP, models.ID] = Depends(backend.get_strategy),
+        ):
+            user = await user_manager.authenticate(credentials)
+
+            if user is None or not user.is_active:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=ErrorCode.LOGIN_BAD_CREDENTIALS,
+                )
+            if requires_verification and not user.is_verified:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=ErrorCode.LOGIN_USER_NOT_VERIFIED,
+                )
+            response = await backend.login(strategy, user)
+            await user_manager.on_after_login(user, request, response)
+
+            # Append user role to the response body if available
+            if hasattr(user, "role"):
+                body_data = json.loads(response.body)
+                body_data["role"] = user.role
+                response.body = json.dumps(body_data).encode("utf-8")
+                response.headers["Content-Length"] = str(len(response.body))
+
+            print(response.body)
+            await user_manager.on_after_login(user, request, response)
+            return response
+
+        logout_responses: OpenAPIResponseType = {
+            **{
+                status.HTTP_401_UNAUTHORIZED: {
+                    "description": "Missing token or inactive user."
+                }
+            },
+            **backend.transport.get_openapi_logout_responses_success(),
+        }
+
+        @router.post(
+            "/logout", name=f"auth:{backend.name}.logout", responses=logout_responses
+        )
+        async def logout(
+                user_token: tuple[models.UP, str] = Depends(get_current_user_token),
+                strategy: Strategy[models.UP, models.ID] = Depends(backend.get_strategy),
+        ):
+            user, token = user_token
+            return await backend.logout(strategy, user, token)
+
+        return router
+
