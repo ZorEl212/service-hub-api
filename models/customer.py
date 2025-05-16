670eacc3bd603630fa1ea9e78739e7110aaa8959
ZorEl212 <yabsirad212@gmail.com>
2025-05-16 06:28:14 -0400

    models: Extend Customer model with address, saved services, and history tracking
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/customer.py
+++ models/customer.py
@@ -1,6 +1,19 @@
-from beanie import Document, Link
+from datetime import datetime
+from typing import List
 
+from beanie import Document, Link
+from pydantic import BaseModel as PydanticModel
+
+from models.service import ServiceItem
 from models.user import User
+from models.attributes import Address
+
+
+class History(PydanticModel):
+    price: float
+    services: Link[ServiceItem]
+    status: str
+    date: datetime
 
 
 class Customer(Document):
@@ -8,4 +21,8 @@ class Customer(Document):
     first_name: str = ""
     last_name: str = ""
     username: str = ""
+    address: Address
+    saved_services: List[Link[ServiceItem]] = []
+    history: List[History] = []
+
 
