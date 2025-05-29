d64277004580ad1ce2058ad9c1b1f0b0e4a053e5
ZorEl212 <yabsirad212@gmail.com>
2025-05-29 05:49:08 -0400

    main: check if env file exists and variables are loaded properly
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import argparse
+import asyncio
+import os
+
+import dotenv
 import uvicorn
 
+
+def check_env():
+    if not dotenv.find_dotenv():
+        raise FileNotFoundError("No .env file found")
+    dotenv.load_dotenv(".env")
+
+async def main():
+    check_env()
+    uvicorn.run("app:app", port=5000, log_level="info", reload=True)
+
+
 if __name__ == "__main__":
-    uvicorn.run("app:app", port=5000, log_level="info")
+    asyncio.run(main())
