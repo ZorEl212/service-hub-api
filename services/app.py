969b8e13fccb1091a1acd93039f98f67c7cbd70e
ZorEl212 <yabsirad212@gmail.com>
2025-05-29 05:50:26 -0400

    services: Add media storage method
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- services/app.py
+++ services/app.py
@@ -7,6 +7,7 @@ from models.engine.file_storage import FileStorage
 class DBSessionContext(object):
     def __init__(self):
         self.db = models.storage
+        self.media_storage = models.media_storage
         print(f"Storage instance: {type(self.db)}")
 
 class AppCRUD(DBSessionContext):
