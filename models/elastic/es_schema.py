5f23d94bba954e6ff8a5029cb7c7204fa13a9bdc
ZorEl212 <yabsirad212@gmail.com>
2025-05-31 02:54:33 -0400

    elastic: Update es schema
    Use proper validated categories
    
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/elastic/es_schema.py
+++ models/elastic/es_schema.py
@@ -3,13 +3,16 @@
 from pydantic import BaseModel
 from typing import List, Optional, Dict
 import models
+from models.attributes import BusinessCategory, Subcategory
+
 
 class ServiceProviderSearchDoc(BaseModel):
     id: str
     name: str
     description: str
     phone: str
-    category_text: Optional[str]
+    category: Optional[BusinessCategory]
+    subcategories: Optional[List[Subcategory]]
     service_titles: List[str]
     service_descriptions: List[str]
     category_titles: List[str]
