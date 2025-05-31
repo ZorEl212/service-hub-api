c718e85548a6763a0ef5a6fa5700b1f4053a1331
ZorEl212 <yabsirad212@gmail.com>
2025-05-31 02:55:07 -0400

    elastic: Update search query schema
    Enforcing category validation
    
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- schemas/generic_schemas.py
+++ schemas/generic_schemas.py
@@ -1,14 +1,18 @@
-from typing import Optional
+from typing import List, Optional
 from pydantic import BaseModel
+
+from models.attributes import BusinessCategory, Subcategory
 
 
 class SearchFilters(BaseModel):
     q: Optional[str] = None
-    category: Optional[str] = None
+    category: Optional[BusinessCategory] = None
+    subcategory: Optional[List[Subcategory]] = None
     location: Optional[str] = None
     rating: Optional[float] = None
     price_min: Optional[float] = None
     price_max: Optional[float] = None
+    distance: Optional[int] = None
     sort: str = "relevance"
     page: int = 1
     limit: int = 10
